86b830b7a90eb20a12dfde1a31bd323659d9eb7a
Dmitry Kn <4dmitry.kn@gmail.com>
2021-03-09 20:50:05 -0500

    cart model updated

--- models/user.js
+++ models/user.js
@@ -2,14 +2,27 @@ const getDB = require('../utils/database').getDB;
 const mongodb = require('mongodb');
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDB();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDB();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
