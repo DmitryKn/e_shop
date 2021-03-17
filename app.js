7fd42071948438149138943d3b8391a195820120
Dmitry Kn <4dmitry.kn@gmail.com>
2021-03-17 13:59:06 -0400

    session init

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const shopRoutes = require('./routes/shopRoutes');
 const adminRoutes = require('./routes/adminRoutes');
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get('Cookie').split(';')[1]);
   res.render('auth/login', {
     isAuthenticated: req.isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
   res.redirect('/');
 };
