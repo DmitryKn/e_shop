cca02c52aed17826e6adefc426d5adc21f5e738c
Dmitry Kn <4dmitry.kn@gmail.com>
2021-03-19 17:15:18 -0400

    no add_to_cart button when logout

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/login', AuthController.getLogin);
 router.post('/login', AuthController.postLogin);
+router.post('/logout', AuthController.postLogout);
 
 module.exports = router;
