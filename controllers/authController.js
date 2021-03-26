cf9ec86c8e6df67fe406d2477c2d27378aaa92ec
Dmitry Kn <4dmitry.kn@gmail.com>
2021-03-26 09:56:01 -0400

    resetting password logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -151,3 +151,62 @@ exports.postReset = (req, res, next) => {
       .catch((err) => console.log(err));
   });
 };
+
+exports.getNewPassword = (req, res, next) => {
+  let token = req.params.token;
+  User.findOne({
+    //user with token and valid time
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => console.log(err));
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/new-password', {
+    errorMessage: message,
+  });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
+    })
+    .catch((err) => console.log(err));
+};
--- routes/auth.js
+++ routes/auth.js
@@ -9,5 +9,7 @@ router.get('/signup', AuthController.getSignup);
 router.post('/signup', AuthController.postSignup);
 router.get('/reset', AuthController.getReset);
 router.post('/reset', AuthController.postReset);
+router.get('/reset/:token', AuthController.getNewPassword);
+router.post('/new-password', AuthController.postNewPassword);
 
 module.exports = router;
